2ea8d85658de7a7ddc7c1875d7e44414bc2a0a33
Shpingalet007 <22795961+shpingalet007@users.noreply.github.com>
2023-05-07 13:38:48 +0200

    Init NPM
    * Install Prettifier and prettify code
    * Install ESLint
    * Gitignore some files

--- main.js
+++ main.js
@@ -1,105 +1,108 @@
 class ElementSelector {
-    canvas;
-    targetElem;
-    interval;
-    toggled;
+  canvas;
+  targetElem;
+  interval;
+  toggled;
 
-    constructor() {
-        this.initListeners();
-        this.constructStyles();
-        this.canvas = this.constructCanvas();
-    }
+  constructor() {
+    this.initListeners();
+    this.constructStyles();
+    this.canvas = this.constructCanvas();
+  }
 
-    async togglePrompt() {
-        const ctx = this.canvas.getContext('2d');
+  async togglePrompt() {
+    const ctx = this.canvas.getContext("2d");
 
-        this.canvas.classList.add('enabled');
+    this.canvas.classList.add("enabled");
 
-        this.toggled = true;
+    this.toggled = true;
 
-        return new Promise((resolve, reject) => {
-            document.addEventListener('click', (e) => {
-                ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-                this.canvas.classList.remove('enabled');
-                this.toggled = false;
-                resolve(this.targetElem);
-            });
-        });
-    }
-
-    detectElement() {
-        const elem = document.elementsFromPoint(this.cursor.x, this.cursor.y)[1];
-
-        if (!elem) {
-            return;
-        }
-
-        this.changeTargetElem(elem);
-        this.drawSelectionBox(elem);
-    }
-
-    changeTargetElem(elem) {
-        if (this.targetElem === elem) {
-            return;
-        }
-
-        if (ElementSelector.isElemAnimated(elem) && !this.interval) {
-            // TODO: Make dynamic screen Hz rendering
-            this.interval = setInterval(() => (
-                this.drawSelectionBox(elem)
-            ), 16);
-        } else {
-            clearInterval(this.interval);
-            this.interval = null;
-        }
-
-        this.targetElem = elem;
-    }
-
-    drawSelectionBox(elem) {
-        const ctx = this.canvas.getContext('2d');
-
-        const elemCoords = elem.getBoundingClientRect();
-
-        const topCoord = Math.round(elemCoords.top) + 0.5;
-        const rightCoord = Math.round(elemCoords.right) + 0.5;
-        const bottomCoord = Math.round(elemCoords.bottom) + 0.5;
-        const leftCoord = Math.round(elemCoords.left) + 0.5;
-
+    return new Promise((resolve, reject) => {
+      document.addEventListener("click", (e) => {
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.canvas.classList.remove("enabled");
+        this.toggled = false;
+        resolve(this.targetElem);
+      });
+    });
+  }
 
-        ctx.setLineDash([3, 2]);
+  detectElement() {
+    const elem = document.elementsFromPoint(this.cursor.x, this.cursor.y)[1];
 
-        ctx.fillStyle = '#569dd941';
-        ctx.fillRect(leftCoord, topCoord, rightCoord - leftCoord, bottomCoord - topCoord);
-
-        ctx.strokeStyle = '#7508cb';
-
-        ctx.beginPath();
-        ctx.moveTo(0, topCoord);
-        ctx.lineTo(this.canvas.width, topCoord);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(0, bottomCoord);
-        ctx.lineTo(this.canvas.width, bottomCoord);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(leftCoord, 0);
-        ctx.lineTo(leftCoord, this.canvas.height);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(rightCoord, 0);
-        ctx.lineTo(rightCoord, this.canvas.height);
-        ctx.stroke();
+    if (!elem) {
+      return;
     }
 
-    constructStyles() {
-        const style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = `
+    this.changeTargetElem(elem);
+    this.drawSelectionBox(elem);
+  }
+
+  changeTargetElem(elem) {
+    if (this.targetElem === elem) {
+      return;
+    }
+
+    if (ElementSelector.isElemAnimated(elem) && !this.interval) {
+      // TODO: Make dynamic screen Hz rendering
+      this.interval = setInterval(() => this.drawSelectionBox(elem), 16);
+    } else {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+
+    this.targetElem = elem;
+  }
+
+  drawSelectionBox(elem) {
+    const ctx = this.canvas.getContext("2d");
+
+    const elemCoords = elem.getBoundingClientRect();
+
+    const topCoord = Math.round(elemCoords.top) + 0.5;
+    const rightCoord = Math.round(elemCoords.right) + 0.5;
+    const bottomCoord = Math.round(elemCoords.bottom) + 0.5;
+    const leftCoord = Math.round(elemCoords.left) + 0.5;
+
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    ctx.setLineDash([3, 2]);
+
+    ctx.fillStyle = "#569dd941";
+    ctx.fillRect(
+      leftCoord,
+      topCoord,
+      rightCoord - leftCoord,
+      bottomCoord - topCoord
+    );
+
+    ctx.strokeStyle = "#7508cb";
+
+    ctx.beginPath();
+    ctx.moveTo(0, topCoord);
+    ctx.lineTo(this.canvas.width, topCoord);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, bottomCoord);
+    ctx.lineTo(this.canvas.width, bottomCoord);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(leftCoord, 0);
+    ctx.lineTo(leftCoord, this.canvas.height);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(rightCoord, 0);
+    ctx.lineTo(rightCoord, this.canvas.height);
+    ctx.stroke();
+  }
+
+  constructStyles() {
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = `
           #element-selector {
             display: none;
             z-index: calc(9e999);
@@ -114,60 +117,64 @@ class ElementSelector {
             display: block;
           }
         `;
-        document.getElementsByTagName('head')[0].appendChild(style);
-    }
+    document.getElementsByTagName("head")[0].appendChild(style);
+  }
 
-    constructCanvas() {
-        const canvas = document.createElement('canvas');
-        canvas.setAttribute('id', 'element-selector');
+  constructCanvas() {
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("id", "element-selector");
 
-        canvas.classList.add('enabled');
+    canvas.classList.add("enabled");
 
-        const lastChild = document.body.lastChild;
-        document.body.insertBefore(canvas, lastChild);
+    const lastChild = document.body.lastChild;
+    document.body.insertBefore(canvas, lastChild);
 
-        canvas.setAttribute('width', canvas.clientWidth.toString());
-        canvas.setAttribute('height', canvas.clientHeight.toString());
+    canvas.setAttribute("width", canvas.clientWidth.toString());
+    canvas.setAttribute("height", canvas.clientHeight.toString());
 
-        canvas.classList.remove('enabled');
+    canvas.classList.remove("enabled");
 
-        return canvas;
-    }
+    return canvas;
+  }
 
-    updateCanvas() {
-        const canvas = document.querySelector('#element-selector');
+  updateCanvas() {
+    const canvas = document.querySelector("#element-selector");
 
-        canvas.setAttribute('width', canvas.clientWidth.toString());
-        canvas.setAttribute('height', canvas.clientHeight.toString());
-    }
+    canvas.setAttribute("width", canvas.clientWidth.toString());
+    canvas.setAttribute("height", canvas.clientHeight.toString());
+  }
 
-    initListeners() {
-        document.addEventListener('mousemove', (e) => {
-            this.cursor = { x: e.clientX, y: e.clientY };
+  initListeners() {
+    document.addEventListener(
+      "mousemove",
+      (e) => {
+        this.cursor = { x: e.clientX, y: e.clientY };
 
-            if (this.toggled) {
-                this.detectElement();
-            }
-        }, { passive: true });
+        if (this.toggled) {
+          this.detectElement();
+        }
+      },
+      { passive: true }
+    );
 
-        document.addEventListener('scroll', (e) => {
-            if (this.toggled) {
-                this.detectElement();
-            }
-        });
+    document.addEventListener("scroll", (e) => {
+      if (this.toggled) {
+        this.detectElement();
+      }
+    });
 
-        document.addEventListener('mouseout', (e) => {
-            this.canvas.classList.remove('enabled');
-        });
+    document.addEventListener("mouseout", (e) => {
+      this.canvas.classList.remove("enabled");
+    });
 
-        document.addEventListener('mouseover', (e) => {
-            this.canvas.classList.add('enabled');
-        });
+    document.addEventListener("mouseover", (e) => {
+      this.canvas.classList.add("enabled");
+    });
 
-        window.addEventListener('resize', (e) => {
-            this.updateCanvas();
-        });
-    }
+    window.addEventListener("resize", (e) => {
+      this.updateCanvas();
+    });
+  }
 
-    static isElemAnimated = (elem) => (elem.getAnimations().length > 0);
+  static isElemAnimated = (elem) => elem.getAnimations().length > 0;
 }
